e833bd0c690f4a32dd4d9e4a43c276598e448f74
tobitobita <tobi_x2@yahoo.co.jp>
2012-10-26 15:26:18 +0900

    リファクタリング

--- code-view-plugin/src/main/java/dsk/codeview/plugin/view/CodeView.java
+++ code-view-plugin/src/main/java/dsk/codeview/plugin/view/CodeView.java
@@ -9,7 +9,6 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.swing.JComboBox;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 
@@ -41,124 +40,128 @@ import dsk.export.ClassExport;
 import dsk.export.tools.SkeletonCodeTools;
 
 public class CodeView implements IPluginExtraTabView, IEntitySelectionListener {
-    private static final Logger LOG = LoggerFactory.getLogger(CodeView.class);
+	private static final Logger LOG = LoggerFactory.getLogger(CodeView.class);
 
-    private RSyntaxTextArea sourceView = new RSyntaxTextArea();
+	private static final String MAX_LANGUAGE_NAME = "Javascript";
 
-    private ClassExport export;
-    private SkeletonCodeTools codeTools;
-    private IDiagramViewManager diagramViewManager;
+	private RSyntaxTextArea sourceView = new RSyntaxTextArea();
 
-    public CodeView() {
-        LOG.trace("CodeView create.");
-        // インスタンス設定
-        try {
-            ProjectAccessor projectAccessor = ProjectAccessorFactory.getProjectAccessor();
-            this.diagramViewManager = projectAccessor.getViewManager().getDiagramViewManager();
-        } catch (ClassNotFoundException e) {
-            LOG.error(e.getMessage(), e);
-        } catch (InvalidUsingException e) {
-            LOG.error(e.getMessage(), e);
-        }
-        Injector injector = Guice.createInjector(Stage.PRODUCTION, new CodeViewModule());
-        this.export = injector.getInstance(ClassExport.class);
-        this.codeTools = injector.getInstance(SkeletonCodeTools.class);
-        // 選択リスナに登録
-        this.diagramViewManager.addEntitySelectionListener(this);
-        // ソース表示の設定
-        this.sourceView.setEditable(false);
-        this.sourceView.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_PHP);
-        this.sourceView.setCodeFoldingEnabled(false);
-        this.sourceView.setAntiAliasingEnabled(true);
-    }
+	private ClassExport export;
+	private SkeletonCodeTools codeTools;
+	private IDiagramViewManager diagramViewManager;
 
-    /* IEntitySelectionListener */
+	public CodeView() {
+		LOG.trace("CodeView create.");
+		// インスタンス設定
+		try {
+			ProjectAccessor projectAccessor = ProjectAccessorFactory
+					.getProjectAccessor();
+			this.diagramViewManager = projectAccessor.getViewManager()
+					.getDiagramViewManager();
+		} catch (ClassNotFoundException e) {
+			LOG.error(e.getMessage(), e);
+		} catch (InvalidUsingException e) {
+			LOG.error(e.getMessage(), e);
+		}
+		Injector injector = Guice.createInjector(Stage.PRODUCTION,
+				new CodeViewModule());
+		this.export = injector.getInstance(ClassExport.class);
+		this.codeTools = injector.getInstance(SkeletonCodeTools.class);
+		// 選択リスナに登録
+		this.diagramViewManager.addEntitySelectionListener(this);
+		// ソース表示の設定
+		this.sourceView.setEditable(false);
+		this.sourceView.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_PHP);
+		this.sourceView.setCodeFoldingEnabled(false);
+		this.sourceView.setAntiAliasingEnabled(true);
+	}
 
-    @Override
-    public void entitySelectionChanged(IEntitySelectionEvent event) {
-        IPresentation[] presentations = this.diagramViewManager.getSelectedPresentations();
-        if (0 >= presentations.length) {
-            return;
-        }
-        IElement model = presentations[0].getModel();
-        if (!this.codeTools.isClass(model)) {
-            return;
-        }
-        IClass clazz = (IClass) model;
-        if (StringUtils.isEmpty(clazz.getName())) {
-            return;
-        }
+	/* IEntitySelectionListener */
 
-        try {
-            String str = this.export.createSkeletonCode(clazz);
-            this.sourceView.setText(str.replace("\t", "  "));
-            this.sourceView.setCaretPosition(0);
-        } catch (IOException e) {
-            LOG.error(e.getMessage(), e);
-        } catch (NoSuchMethodError e) {
-            LOG.error(e.getMessage(), e);
-            JOptionPane.showMessageDialog(null, "Code View Pluginは、6.6.3以下のバージョンには対応していません",
-                    "Alert", JOptionPane.ERROR_MESSAGE);
-        }
-    }
+	@Override
+	public void entitySelectionChanged(IEntitySelectionEvent event) {
+		IPresentation[] presentations = this.diagramViewManager
+				.getSelectedPresentations();
+		if (0 >= presentations.length) {
+			return;
+		}
+		IElement model = presentations[0].getModel();
+		if (!this.codeTools.isClass(model)) {
+			return;
+		}
+		IClass clazz = (IClass) model;
+		if (StringUtils.isEmpty(clazz.getName())) {
+			return;
+		}
 
-    /* IPluginExtraTabView */
+		try {
+			String str = this.export.createSkeletonCode(clazz);
+			this.sourceView.setText(str.replace("\t", "  "));
+			this.sourceView.setCaretPosition(0);
+		} catch (IOException e) {
+			LOG.error(e.getMessage(), e);
+		} catch (NoSuchMethodError e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
 
-    @Override
-    public void activated() {
-    }
+	/* IPluginExtraTabView */
 
-    @Override
-    public void addSelectionListener(ISelectionListener listener) {
-    }
+	@Override
+	public void activated() {
+	}
 
-    @Override
-    public void deactivated() {
-    }
+	@Override
+	public void addSelectionListener(ISelectionListener listener) {
+	}
 
-    @Override
-    public Component getComponent() {
-        JPanel contentPane = new JPanel(new BorderLayout(0, 0));
-        // バー
-        JPanel barPane = new JPanel(new BorderLayout(0, 0));
-        contentPane.add(barPane, BorderLayout.NORTH);
-        JToolBar toolBar = new JToolBar();
-        barPane.add(toolBar, BorderLayout.EAST);
-        toolBar.setAlignmentY(Component.CENTER_ALIGNMENT);
-        toolBar.setFloatable(false);
-        JComboBox comboBox = new JComboBox(this.selectedKind());
-        comboBox.setEditable(false);
-        comboBox.setPrototypeDisplayValue("Javascript");
-        comboBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // TODO 出力選択変更
-                // JComboBox comboBox = (JComboBox) e.getSource();
-                // System.out.println(comboBox.getSelectedItem().toString());
-            }
-        });
-        toolBar.add(comboBox);
-        // 表示エリア
-        RTextScrollPane sp = new RTextScrollPane(this.sourceView);
-        sp.setFoldIndicatorEnabled(true);
-        contentPane.add(sp);
-        return contentPane;
-    }
+	@Override
+	public void deactivated() {
+	}
 
-    /**
-     * TODO 出力の種類
-     */
-    private String[] selectedKind() {
-        return new String[] { "PHP" };
-    }
+	@Override
+	public Component getComponent() {
+		JPanel contentPane = new JPanel(new BorderLayout(0, 0));
+		// バー
+		JPanel barPane = new JPanel(new BorderLayout(0, 0));
+		contentPane.add(barPane, BorderLayout.NORTH);
+		JToolBar toolBar = new JToolBar();
+		barPane.add(toolBar, BorderLayout.EAST);
+		toolBar.setAlignmentY(Component.CENTER_ALIGNMENT);
+		toolBar.setFloatable(false);
+		JComboBox comboBox = new JComboBox(this.selectedKind());
+		comboBox.setEditable(false);
+		comboBox.setPrototypeDisplayValue(MAX_LANGUAGE_NAME);
+		comboBox.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 出力選択変更
+				// JComboBox comboBox = (JComboBox) e.getSource();
+				// System.out.println(comboBox.getSelectedItem().toString());
+			}
+		});
+		toolBar.add(comboBox);
+		// 表示エリア
+		RTextScrollPane sp = new RTextScrollPane(this.sourceView);
+		sp.setFoldIndicatorEnabled(true);
+		contentPane.add(sp);
+		return contentPane;
+	}
 
-    @Override
-    public String getDescription() {
-        return null;
-    }
+	/**
+	 * TODO 出力の種類
+	 */
+	private String[] selectedKind() {
+		return new String[] { "PHP" };
+	}
 
-    @Override
-    public String getTitle() {
-        return R.m(NAME, "Code　View");
-    }
+	@Override
+	public String getDescription() {
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		return R.m(NAME, "Code　View");
+	}
 }
